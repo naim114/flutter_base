c811874a4f1758789f66556d70a40030ef18e892
naim114 <naim.muhammad99@gmail.com>
2023-04-04 20:32:07 +0800

    Add futurebuilder at main to act as loading builder

--- lib/main.dart
+++ lib/main.dart
@@ -109,7 +109,24 @@ class _MyAppState extends State<MyApp> {
                     iconColor: MaterialStatePropertyAll(CustomColor.neutral1)),
               ),
             ),
-            home: const Wrapper(),
+            home: FutureBuilder(
+              future: Future.delayed(const Duration(seconds: 2)),
+              builder: (context, snapshot) {
+                return snapshot.connectionState == ConnectionState.waiting
+                    ? Scaffold(
+                        body: Center(
+                          child: Center(
+                            child: LoadingAnimationWidget.flickr(
+                              leftDotColor: CustomColor.primary,
+                              rightDotColor: CustomColor.secondary,
+                              size: 50,
+                            ),
+                          ),
+                        ),
+                      )
+                    : Wrapper();
+              },
+            ),
           ),
         );
       },
