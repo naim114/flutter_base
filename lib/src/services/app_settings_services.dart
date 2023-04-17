6e36a35d074ef3e9b896d96fcf544de8e41b13dd
naim114 <naim.muhammad99@gmail.com>
2023-04-17 16:19:43 +0800

    Create model class and service class for application service

---
+++ lib/src/services/app_settings_services.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_base/src/model/app_settings_model.dart';
+import 'package:network_info_plus/network_info_plus.dart';
+
+class AppSettingsServices {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final CollectionReference<Map<String, dynamic>> collectionRef =
+      FirebaseFirestore.instance.collection('Settings');
+
+  final FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
+
+  static final DeviceInfoPlugin _deviceInfoPlugin = DeviceInfoPlugin();
+  final NetworkInfo _networkInfo = NetworkInfo();
+
+  Stream<AppSettingsModel> getAppSettingsStream() {
+    final DocumentReference<Map<String, dynamic>> documentRef =
+        collectionRef.doc('application');
+
+    return documentRef.snapshots().map((snapshot) {
+      final data = snapshot.data();
+      return AppSettingsModel.fromMap(data!);
+    });
+  }
+}
