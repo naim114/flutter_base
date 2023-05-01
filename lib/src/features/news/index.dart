99a83a9d677f1a9fdd83595c1459894b01484d81
naim114 <naim.muhammad99@gmail.com>
2023-05-01 21:29:26 +0800

    Rework refreshindicator

--- lib/src/features/news/index.dart
+++ lib/src/features/news/index.dart
@@ -24,31 +24,39 @@ class _NewsState extends State<News> with TickerProviderStateMixin {
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
       GlobalKey<RefreshIndicatorState>();
 
-  List<List<NewsModel?>> allList = [
+  List<List<NewsModel>> allList = [
     [], // fetchedStarredNews
     [], // fetchedPopularNews
     [], // fetchedLatestNews
   ];
+  bool loading = true;
 
   Future<void> _refreshData() async {
     try {
-      final List<NewsModel?> fetchedStarredNews = await NewsService().getAllBy(
+      final List<NewsModel> fetchedStarredNews = (await NewsService().getAllBy(
         fieldName: 'starred',
         desc: true,
         limit: 5,
-      );
-      final List<NewsModel?> fetchedPopularNews = await NewsService().getAllBy(
+      ))
+          .whereType<NewsModel>()
+          .toList();
+      final List<NewsModel> fetchedPopularNews = (await NewsService().getAllBy(
         fieldName: 'likeCount',
         desc: true,
         limit: 5,
-      );
-      final List<NewsModel?> fetchedLatestNews = await NewsService().getAllBy(
+      ))
+          .whereType<NewsModel>()
+          .toList();
+      final List<NewsModel> fetchedLatestNews = (await NewsService().getAllBy(
         fieldName: 'createdAt',
         desc: true,
         limit: 5,
-      );
+      ))
+          .whereType<NewsModel>()
+          .toList();
 
       setState(() {
+        loading = false;
         allList = [fetchedStarredNews, fetchedPopularNews, fetchedLatestNews];
       });
 
@@ -62,127 +70,108 @@ class _NewsState extends State<News> with TickerProviderStateMixin {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _refreshData();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final appSettings = Provider.of<AppSettingsModel?>(context);
 
-    return appSettings == null && widget.user == null
+    return (appSettings == null && widget.user == null) || loading
         ? const Scaffold(body: Center(child: CircularProgressIndicator()))
         : RefreshIndicator(
             key: _refreshIndicatorKey,
             onRefresh: _refreshData,
-            child: FutureBuilder(
-              future: Future.wait([
-                NewsService().getAllBy(
-                  fieldName: 'starred',
-                  desc: true,
-                  limit: 5,
-                ),
-                NewsService().getAllBy(
-                  fieldName: 'likedBy',
-                  desc: true,
-                  limit: 5,
-                ),
-                NewsService().getAllBy(
-                  fieldName: 'createdAt',
-                  desc: true,
-                  limit: 5,
-                ),
-              ]),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Scaffold(
-                      body: Center(child: CircularProgressIndicator()));
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else {
-                  final List<NewsModel?> starredNewsList = snapshot.data![0];
-                  final List<NewsModel?> popularNewsList = snapshot.data![1];
-                  final List<NewsModel?> latestNewsList = snapshot.data![2];
+            child: Builder(
+              builder: (context) {
+                final List<NewsModel?> starredNewsList = allList[0];
+                final List<NewsModel?> popularNewsList = allList[1];
+                final List<NewsModel?> latestNewsList = allList[2];
 
-                  return Scaffold(
-                    appBar: AppBar(
-                      centerTitle: true,
-                      actions: [
-                        IconButton(
-                          onPressed: () async {
-                            showDialog(
-                              context: widget.mainContext,
-                              barrierDismissible: false,
-                              builder: (BuildContext context) {
-                                return const Center(
-                                  child: CircularProgressIndicator(),
-                                );
-                              },
-                            );
+                return Scaffold(
+                  appBar: AppBar(
+                    centerTitle: true,
+                    actions: [
+                      IconButton(
+                        onPressed: () async {
+                          showDialog(
+                            context: widget.mainContext,
+                            barrierDismissible: false,
+                            builder: (BuildContext context) {
+                              return const Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            },
+                          );
 
-                            await NewsService().searchNews(
-                                context: widget.mainContext,
-                                user: widget.user!);
+                          await NewsService().searchNews(
+                              context: widget.mainContext, user: widget.user!);
 
-                            if (context.mounted) {
-                              Navigator.of(widget.mainContext,
-                                      rootNavigator: true)
-                                  .pop();
-                            }
-                          },
-                          icon: const Icon(Icons.search),
-                        )
-                      ],
-                      title: CachedNetworkImage(
-                        imageUrl: appSettings!.logoFaviconURL,
+                          if (context.mounted) {
+                            Navigator.of(widget.mainContext,
+                                    rootNavigator: true)
+                                .pop();
+                          }
+                        },
+                        icon: const Icon(Icons.search),
+                      )
+                    ],
+                    title: CachedNetworkImage(
+                      imageUrl: appSettings!.logoFaviconURL,
+                      fit: BoxFit.contain,
+                      height: 30,
+                      placeholder: (context, url) => Image.asset(
+                        'assets/images/default_logo_favicon.png',
                         fit: BoxFit.contain,
                         height: 30,
-                        placeholder: (context, url) => Image.asset(
-                          'assets/images/default_logo_favicon.png',
-                          fit: BoxFit.contain,
-                          height: 30,
-                        ),
-                        errorWidget: (context, url, error) => Image.asset(
-                          'assets/images/default_logo_favicon.png',
-                          fit: BoxFit.contain,
-                          height: 30,
-                        ),
+                      ),
+                      errorWidget: (context, url, error) => Image.asset(
+                        'assets/images/default_logo_favicon.png',
+                        fit: BoxFit.contain,
+                        height: 30,
                       ),
                     ),
-                    body: ListView(
-                      children: [
-                        const SizedBox(height: 5),
-                        starredNewsList.isEmpty
-                            ? const SizedBox()
-                            : newsSection(
-                                context: context,
-                                mainContext: widget.mainContext,
-                                newsList: starredNewsList,
-                                user: widget.user!,
-                                title: 'More News',
-                              ),
-                        const SizedBox(height: 5),
-                        // Popular News Cards
-                        popularNewsList.isEmpty
-                            ? const SizedBox()
-                            : newsSection(
-                                context: context,
-                                mainContext: widget.mainContext,
-                                newsList: popularNewsList,
-                                user: widget.user!,
-                                title: 'Popular News',
-                              ),
-                        const SizedBox(height: 5),
-                        // Latest News Cards
-                        latestNewsList.isEmpty
-                            ? const SizedBox()
-                            : newsSection(
-                                context: context,
-                                mainContext: widget.mainContext,
-                                newsList: latestNewsList,
-                                user: widget.user!,
-                                title: 'Latest News',
-                              ),
-                        const SizedBox(height: 40),
-                      ],
-                    ),
-                  );
-                }
+                  ),
+                  body: ListView(
+                    children: [
+                      const SizedBox(height: 5),
+                      starredNewsList.isEmpty
+                          ? const SizedBox()
+                          : newsSection(
+                              context: context,
+                              mainContext: widget.mainContext,
+                              newsList: starredNewsList,
+                              user: widget.user!,
+                              title: 'More News',
+                            ),
+                      const SizedBox(height: 5),
+                      // Popular News Cards
+                      popularNewsList.isEmpty
+                          ? const SizedBox()
+                          : newsSection(
+                              context: context,
+                              mainContext: widget.mainContext,
+                              newsList: popularNewsList,
+                              user: widget.user!,
+                              title: 'Popular News',
+                            ),
+                      const SizedBox(height: 5),
+                      // Latest News Cards
+                      latestNewsList.isEmpty
+                          ? const SizedBox()
+                          : newsSection(
+                              context: context,
+                              mainContext: widget.mainContext,
+                              newsList: latestNewsList,
+                              user: widget.user!,
+                              title: 'Latest News',
+                            ),
+                      const SizedBox(height: 40),
+                    ],
+                  ),
+                );
               },
             ),
           );
--- lib/src/features/notification/index.dart
+++ lib/src/features/notification/index.dart
@@ -23,16 +23,20 @@ class _NotificationsState extends State<Notifications> {
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
       GlobalKey<RefreshIndicatorState>();
 
-  List<NotificationModel?> notiList = List.empty(growable: true);
+  List<NotificationModel> notiList = List.empty(growable: true);
+  bool loading = true;
 
   Future<void> _refreshData() async {
     try {
       // Call the asynchronous operation to fetch data
-      final List<NotificationModel?> fetchedData =
-          await NotificationServices().getBy('receiver', widget.user!.id);
+      final List<NotificationModel> fetchedData =
+          (await NotificationServices().getBy('receiver', widget.user!.id))
+              .whereType<NotificationModel>()
+              .toList();
 
       // Update the state with the fetched data and call setState to rebuild the UI
       setState(() {
+        loading = false;
         notiList = fetchedData;
       });
 
@@ -46,130 +50,122 @@ class _NotificationsState extends State<Notifications> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _refreshData();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return widget.user == null
+    return widget.user == null || loading
         ? const Scaffold(body: Center(child: CircularProgressIndicator()))
         : RefreshIndicator(
             key: _refreshIndicatorKey,
             onRefresh: _refreshData,
             child: Scaffold(
-              body: FutureBuilder<List<NotificationModel?>>(
-                future:
-                    NotificationServices().getBy('receiver', widget.user!.id),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting ||
-                      snapshot.data == null) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.data != null) {
-                    List<NotificationModel> dataList =
-                        snapshot.data!.whereType<NotificationModel>().toList();
-
-                    return ListView(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.only(
-                            top: 25,
-                            left: 25,
-                            right: 25,
-                            bottom: 10,
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              pageTitleIcon(
-                                context: context,
-                                title: "Notification",
-                                icon: const Icon(
-                                  CupertinoIcons.bell_fill,
-                                  size: 20,
-                                ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.symmetric(vertical: 10),
-                                child: Text(
-                                  'View all your notifications. Slide notification for action.',
-                                  style: TextStyle(),
-                                ),
-                              ),
-                            ],
-                          ),
+              body: Builder(
+                builder: (context) {
+                  return ListView(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(
+                          top: 25,
+                          left: 25,
+                          right: 25,
+                          bottom: 10,
                         ),
-                        Column(
-                          children: List.generate(dataList.length, (index) {
-                            NotificationModel noti = dataList[index];
-                            return listTileNotification(
-                              onDelete: (context) {
-                                showDialog<String>(
-                                  context: context,
-                                  builder: (BuildContext context) =>
-                                      AlertDialog(
-                                    title: const Text('Delete Notification?'),
-                                    content: const Text(
-                                        'Are you sure you want to delete this notification? Deleted data may can\'t be retrieved back. Select OK to confirm.'),
-                                    actions: <Widget>[
-                                      TextButton(
-                                        onPressed: () => Navigator.pop(context),
-                                        child: const Text(
-                                          'Cancel',
-                                          style: TextStyle(
-                                            color: CupertinoColors.systemGrey,
-                                          ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            pageTitleIcon(
+                              context: context,
+                              title: "Notification",
+                              icon: const Icon(
+                                CupertinoIcons.bell_fill,
+                                size: 20,
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(vertical: 10),
+                              child: Text(
+                                'View all your notifications. Slide notification for action.',
+                                style: TextStyle(),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Column(
+                        children: List.generate(notiList.length, (index) {
+                          NotificationModel noti = notiList[index];
+                          return listTileNotification(
+                            onDelete: (context) {
+                              showDialog<String>(
+                                context: context,
+                                builder: (BuildContext context) => AlertDialog(
+                                  title: const Text('Delete Notification?'),
+                                  content: const Text(
+                                      'Are you sure you want to delete this notification? Deleted data may can\'t be retrieved back. Select OK to confirm.'),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      child: const Text(
+                                        'Cancel',
+                                        style: TextStyle(
+                                          color: CupertinoColors.systemGrey,
                                         ),
-                                      ),
-                                      TextButton(
-                                        onPressed: () async {
-                                          final result =
-                                              await NotificationServices()
-                                                  .delete(notification: noti);
-
-                                          print("Delete: $result");
-
-                                          if (result == true &&
-                                              context.mounted) {
-                                            Navigator.pop(context);
-                                            Fluttertoast.showToast(
-                                                msg: "Notification Deleted");
-                                            _refreshData();
-                                          }
-                                        },
-                                        child: const Text(
-                                          'OK',
-                                          style: TextStyle(
-                                            color: CustomColor.danger,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              },
-                              onTap: () async {
-                                final result = await NotificationServices()
-                                    .read(notification: noti);
-
-                                print("Read: $result");
-
-                                if (context.mounted) {
-                                  Navigator.push(
-                                    widget.mainContext,
-                                    MaterialPageRoute(
-                                      builder: (context) => NotificationView(
-                                        notification: noti,
                                       ),
                                     ),
-                                  );
-                                  _refreshData();
-                                }
-                              },
-                              noti: noti,
-                            );
-                          }),
-                        ),
-                      ],
-                    );
-                  }
+                                    TextButton(
+                                      onPressed: () async {
+                                        final result =
+                                            await NotificationServices()
+                                                .delete(notification: noti);
 
-                  return const Center(child: CircularProgressIndicator());
+                                        print("Delete: $result");
+
+                                        if (result == true && context.mounted) {
+                                          Navigator.pop(context);
+                                          Fluttertoast.showToast(
+                                              msg: "Notification Deleted");
+                                          _refreshData();
+                                        }
+                                      },
+                                      child: const Text(
+                                        'OK',
+                                        style: TextStyle(
+                                          color: CustomColor.danger,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                            onTap: () async {
+                              final result = await NotificationServices()
+                                  .read(notification: noti);
+
+                              print("Read: $result");
+
+                              if (context.mounted) {
+                                Navigator.push(
+                                  widget.mainContext,
+                                  MaterialPageRoute(
+                                    builder: (context) => NotificationView(
+                                      notification: noti,
+                                    ),
+                                  ),
+                                );
+                                _refreshData();
+                              }
+                            },
+                            noti: noti,
+                          );
+                        }),
+                      ),
+                    ],
+                  );
                 },
               ),
             ),
